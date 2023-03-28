71a3e728e00208c38b11e61d992e77c672f65380
ashishp911 <ashish.patil35@gmail.com>
2023-03-27 21:21:32 -0400

    Header kindof done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import "./App.css";
 import Header from "./components/Header";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import CoinPage from "./pages/CoinPage";
+import { styled } from "@mui/system";
 
 function App() {
+  
+  const MyComponent = styled('div')({
+    backgroundColor: '#14161a',
+    color: 'white',
+    minHeight:"100vh",
+    padding: 8,
+    borderRadius: 4,
+  });
+
+  // const classes = useStyles()
   return (
     <>
-      <Header />
-      <div>
-        <Routes>
-          <Route exact path="/" element={<Homepage />} />
-          <Route exact path="/coins/:id" element={<CoinPage />} />
-        </Routes>
-      </div>
+      <BrowserRouter>
+        <MyComponent>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Homepage />} />
+            <Route exact path="/coins/:id" element={<CoinPage />} />
+          </Routes>
+        </MyComponent>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,67 @@
-import React from 'react'
+import React from "react";
+import { styled } from "@mui/system";
+import {
+  AppBar,
+  Container,
+  Typography,
+  Toolbar,
+  Select,
+  MenuItem,
+  createTheme,
+  ThemeProvider
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <div>
-      In header page
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Header
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      mode: "dark",
+    },
+  });
+
+  const TextHeading = styled(Typography)({
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontSize: "2rem",
+    fontWeight: "bold",
+    cursor: "pointer",
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <TextHeading
+              onClick={() => {
+                navigate("/");
+              }}
+              variant="h6"
+            >
+              CryptoPilot
+            </TextHeading>
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15,
+              }}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
+
+export default Header;
