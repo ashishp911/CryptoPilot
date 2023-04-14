1f2c20324cfcc5a2c8a2d9749bb870b3fc54e517
ashishp911 <ashish.patil35@gmail.com>
2023-04-14 17:29:38 -0400

    Created Coin_Info and called in CoinPage

---
+++ src/components/Coin_Info.js
@@ -0,0 +1,138 @@
+import React, { useEffect, useState } from "react";
+import { CryptoState } from "../CryptoContext";
+import {
+  ThemeProvider,
+  Typography,
+  LinearProgress,
+  useMediaQuery,
+  createTheme,
+} from "@mui/material";
+import HTMLReactParser from "html-react-parser";
+import { numberWithCommas } from "../components/banner/Carousel";
+
+const Coin_Info = (props) => {
+    const { currency, symbol } = CryptoState();
+  const theme = createTheme({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 960,
+        lg: 1280,
+        xl: 1920,
+      },
+    },
+  });
+  const issmScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isxsScreen = useMediaQuery(theme.breakpoints.down("xs"));
+  const isMdScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const sidebarStyles = {
+    width: "30%",
+    display: "flex",
+    flexDirection: "column",
+    ...(isMdScreen && {
+      width: "100%",
+    }),
+    alignItems: "center",
+    marginTop: 25,
+    borderRight: "2px solid grey",
+  };
+
+  const headingStyle = {
+    fontWeight: "bold",
+    marginBottom: 30,
+    fontFamily: "Montserrat",
+  };
+
+  const descriptionStyle = {
+    width: "100%",
+    fontFamily: "Montserrat",
+    padding: 25,
+    paddingBottom: 15,
+    paddingTop: 0,
+    textAlign: "justify",
+  };
+
+  const marketData = {
+    alignSelf: "start",
+    padding: 25,
+    paddingTop: 10,
+    width: "100%",
+    ...(isMdScreen && {
+      display: "flex",
+      justifyContent: "space-around",
+    }),
+    ...(issmScreen && {
+      flexDirection: "column",
+      alignItems: "center",
+    }),
+    ...(isxsScreen && {
+      alignItems: "start",
+    }),
+  };
+
+  return (
+    <div style={sidebarStyles}>
+      <img
+        src={props.coin?.image.large}
+        alt={props.coin?.name}
+        height="200"
+        style={{ marginBottom: 30 }}
+      />
+      <Typography variant="h3" style={headingStyle}>
+        {props.coin?.name}
+      </Typography>
+
+      <Typography variant="subtitles" style={descriptionStyle}>
+        {HTMLReactParser(props.coin?.description.en.split(". ")[0])}
+      </Typography>
+      <div style={marketData}>
+        <span style={{ display: "flex" }}>
+          <Typography variant="h5" style={headingStyle}>
+            Rank :
+          </Typography>
+          <Typography
+            variant="h5"
+            style={{ marginLeft: 10, fontFamily: "Montserrat" }}
+          >
+            {" "}
+            {props.coin?.market_cap_rank}
+          </Typography>
+        </span>
+        <span style={{ display: "flex" }}>
+          <Typography variant="h5" style={headingStyle}>
+            Current Price :
+          </Typography>
+          <Typography
+            variant="h5"
+            style={{ marginLeft: 10, fontFamily: "Montserrat" }}
+          >
+            {symbol}{" "}
+            {numberWithCommas(
+              props.coin?.market_data.current_price[currency.toLowerCase()]
+            )}
+          </Typography>
+        </span>
+        <span style={{ display: "flex" }}>
+          <Typography variant="h5" style={headingStyle}>
+            Market Cap :{" "}
+          </Typography>
+          <Typography
+            variant="h5"
+            style={{ marginLeft: 10, fontFamily: "Montserrat" }}
+          >
+            {symbol}{" "}
+            {numberWithCommas(
+              props.coin?.market_data.market_cap[currency.toLowerCase()]
+                .toString()
+                .slice(0, -6)
+            )}{" "}
+            M
+          </Typography>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Coin_Info;
--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -1,18 +1,16 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { CryptoState } from "../CryptoContext";
-import axios from "axios";
 import { SingleCoin } from "../config/api";
 import {
   ThemeProvider,
-  Typography,
   LinearProgress,
   useMediaQuery,
   createTheme,
 } from "@mui/material";
 import CoinChart from "../components/CoinChart";
-import HTMLReactParser from "html-react-parser";
-import { numberWithCommas } from "../components/banner/Carousel";
+import Coin_Info from "../components/Coin_Info";
 
 const CoinPage = () => {
   const { id } = useParams();
@@ -23,9 +21,6 @@ const CoinPage = () => {
     const { data } = await axios.get(SingleCoin(id));
     setCoin(data);
   };
-
-  // console.log("YOU ARE HRERE");
-  // console.log(coin);
 
   useEffect(() => {
     fetchCoin();
@@ -42,10 +37,8 @@ const CoinPage = () => {
       },
     },
   });
-  const issmScreen = useMediaQuery(theme.breakpoints.down("sm"));
-  const isxsScreen = useMediaQuery(theme.breakpoints.down("xs"));
-  const isMdScreen = useMediaQuery(theme.breakpoints.down("md"));
 
+  const isMdScreen = useMediaQuery(theme.breakpoints.down("md"));
   const containerStyles = {
     display: "flex",
     ...(isMdScreen && {
@@ -55,117 +48,14 @@ const CoinPage = () => {
     marginTop: 50,
   };
 
-  const sidebarStyles = {
-    width: "30%",
-    display: "flex",
-    flexDirection: "column",
-    ...(isMdScreen && {
-      width:'100%',
-    }),
-    alignItems: "center",
-    marginTop: 25,
-    borderRight: "2px solid grey",
-  };
-
-  const headingStyle = {
-    fontWeight: "bold",
-    marginBottom: 30,
-    fontFamily: "Montserrat",
-  };
-
-  const descriptionStyle = {
-    width: "100%",
-    fontFamily: "Montserrat",
-    padding: 25,
-    paddingBottom: 15,
-    paddingTop: 0,
-    textAlign: "justify",
-  };
-
-  const marketData = {
-    alignSelf: "start",
-    padding: 25,
-    paddingTop: 10,
-    width: "100%",
-    ...(isMdScreen && {
-      display:"flex", 
-      justifyContent:"space-around",
-    }),
-    ...(issmScreen && {
-      flexDirection: "column",
-      alignItems:"center"
-    }),
-    ...(isxsScreen && {
-      alignItems:"start"
-    }),
-
-  };
 
   if (!coin) return <LinearProgress style={{ backgroundColor: "gold" }} />;
 
   return (
     <ThemeProvider theme={theme}>
       <div style={containerStyles}>
-        <div style={sidebarStyles}>
-          <img
-            src={coin?.image.large}
-            alt={coin?.name}
-            height="200"
-            style={{ marginBottom: 30 }}
-          />
-          <Typography variant="h3" style={headingStyle}>
-            {coin?.name}
-          </Typography>
-
-          <Typography variant="subtitles" style={descriptionStyle}>
-            {HTMLReactParser(coin?.description.en.split(". ")[0])}
-          </Typography>
-          <div style={marketData}>
-            <span style={{ display: "flex" }}>
-              <Typography variant="h5" style={headingStyle}>
-                Rank :
-              </Typography>
-              <Typography
-                variant="h5"
-                style={{ marginLeft: 10, fontFamily: "Montserrat" }}
-              >
-                {" "}
-                {coin?.market_cap_rank}
-              </Typography>
-            </span>
-            <span style={{ display: "flex" }}>
-              <Typography variant="h5" style={headingStyle}>
-                Current Price :
-              </Typography>
-              <Typography
-                variant="h5"
-                style={{ marginLeft: 10, fontFamily: "Montserrat" }}
-              >
-                {symbol}{" "}
-                {numberWithCommas(
-                  coin?.market_data.current_price[currency.toLowerCase()]
-                )}
-              </Typography>
-            </span>
-            <span style={{ display: "flex" }}>
-              <Typography variant="h5" style={headingStyle}>
-                Market Cap :{" "}
-              </Typography>
-              <Typography
-                variant="h5"
-                style={{ marginLeft: 10, fontFamily: "Montserrat" }}
-              >
-                {symbol}{" "}
-                {numberWithCommas(
-                  coin?.market_data.market_cap[currency.toLowerCase()]
-                    .toString()
-                    .slice(0, -6)
-                )}{" "}
-                M
-              </Typography>
-            </span>
-          </div>
-        </div>
+        {/* Coin Info */}
+        <Coin_Info coin = {coin}/>
         {/* Chart */}
         <CoinChart coin = {coin}/>
       </div>
