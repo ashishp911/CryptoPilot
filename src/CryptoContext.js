9992d6b429046348f39e65e08af1b278ebdbb6ea
ashishp911 <ashish.patil35@gmail.com>
2023-05-17 11:53:13 -0500

    1. moved some vars ad funcs to cryptostate.

--- src/CryptoContext.js
+++ src/CryptoContext.js
@@ -1,16 +1,27 @@
 import React, { Children, createContext, useContext, useState, useEffect } from "react";
-
+import axios from "axios";
+import { CoinList } from "./config/api";
 const CryptoContext = ({ children }) => {
   const [currency, setCurrency] = useState("INR");
   const [symbol, setSymbol] = useState("₹");
-  
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchCoins = async () => {
+    setLoading(true);
+    const { data } = await axios.get(CoinList(currency));
+    setCoins(data);
+    setLoading(false);
+  };
+
+
   useEffect(() => {
     if (currency === "INR") setSymbol("₹");
     else if (currency === "USD") setSymbol("$");
   }, [currency]);
   
   return (
-    <Crypto.Provider value={{ currency, symbol, setCurrency }}>
+    <Crypto.Provider value={{ currency, symbol, setCurrency, coins, loading, fetchCoins }}>
       {children}
     </Crypto.Provider>
   );
--- src/components/Coins_table.js
+++ src/components/Coins_table.js
@@ -23,19 +23,11 @@ import { numberWithCommas } from "./banner/Carousel";
 
 const Coins_table = () => {
   const navigate = useNavigate();
-  const [coins, setCoins] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
   //   For pagination
   const [page, setPage] = useState(1);
-  const { currency, symbol } = CryptoState();
+  const { currency, symbol, coins, loading, fetchCoins } = CryptoState();
 
-  const fetchCoins = async () => {
-    setLoading(true);
-    const { data } = await axios.get(CoinList(currency));
-    setCoins(data);
-    setLoading(false);
-  };
 
   useEffect(() => {
     fetchCoins();
