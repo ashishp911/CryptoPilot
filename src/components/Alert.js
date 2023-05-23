c4547c7f790452ee442646d545828d65b33ef5bb
ashishp911 <ashish.patil35@gmail.com>
2023-05-23 17:09:56 -0500

    error fixed for snackbar

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,38 +1,37 @@
-import React from 'react'
-import { CryptoState } from '../CryptoContext'
-import { Snackbar } from '@mui/material'
-import MuiAlert from '@mui/material/Alert';
+import React from "react";
+import { CryptoState } from "../CryptoContext";
+import { Snackbar } from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
 
 const Alert = () => {
-    const {alert, setAlert} = CryptoState()
+  const { alert, setAlert } = CryptoState();
 
-      const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-          return;
-        }
-    
-        setAlert({open:false})
-  return (
-    <div>
-        <Snackbar 
-        open={alert.open}
-        autoHideDuration={3000}
-        onClose={handleClose}
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
 
+    setAlert({ open: false });
+  };
+  
+    return (
+      <div>
+        <Snackbar
+          open={alert.open}
+          autoHideDuration={3000}
+          onClose={handleClose}
         >
-            <MuiAlert
+          <MuiAlert
             onClose={handleClose}
             elevation={10}
-            variant='filled'
+            variant="filled"
             severity={alert.type}
-
-            >
-                {alert.message}
-            </MuiAlert>
+          >
+            {alert.message}
+          </MuiAlert>
         </Snackbar>
-    </div>
-  )
-}
-}
+      </div>
+    );
+};
 
-export default Alert
+export default Alert;
