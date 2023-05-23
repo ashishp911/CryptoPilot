d5b5d580d5801fda4d86c5fc62e0179a5487194a
ashishp911 <ashish.patil35@gmail.com>
2023-05-23 14:25:05 -0500

    created login and signup popups

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import { TextField, Button } from "@mui/material";
+import React from "react";
+import { useState } from "react";
+import { Box } from "@mui/system";
 
-const Login = ({handleClose}) => {
+const Login = ({ handleClose }) => {
+  // states
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = () => {};
   return (
-    <div>
-      Login page
-    </div>
-  )
-}
+    <Box
+      p={3}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+      }}
+    >
+      <TextField
+        label="Enter your email"
+        name="email"
+        value={email}
+        variant="outlined"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        fullWidth
+      />
 
-export default Login
+      <TextField
+        label="Enter your password"
+        name="password"
+        value={password}
+        variant="outlined"
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        fullWidth
+      />
+      <Button
+      variant="contained"
+      size="large"
+      style={{
+        backgroundColor: "#EEBC1D"
+      }}
+      onClick={handleSubmit}
+      >
+        Login 
+      </Button>
+    </Box>
+  );
+};
+
+export default Login;
--- src/components/authentication/Signup.js
+++ src/components/authentication/Signup.js
@@ -1,61 +1,67 @@
-import { Button, TextField } from '@mui/material'
-import { Box } from '@mui/system'
-import React from 'react'
-import { useState } from 'react'
+import { Button, TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { useState } from "react";
 
-const Signup = ({handleClose}) => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [confirmPassword, setConfirmPassword] = useState("")
+const Signup = ({ handleClose }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const handleSubmit = () => {
+    // 1st check --> if password != confirm password
+    if (password !== confirmPassword) {
+      // using snackbar from material UI as an alert
+    }
+  };
+  return (
+    <Box
+      p={3}
+      style={{ display: "flex", flexDirection: "column", gap: "20px" }}
+    >
+      <TextField
+        variant="outlined"
+        type="email"
+        label="Enter email"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        fullWidth
+      />
 
-  }
-    return (
-    <Box p={3}
-    style = {{display:"flex", flexDirection:"column", gap:"20px"}}>
-    
-    <TextField 
-    variant='outlined'
-    type='email'
-    label="enter email"
-    value={email}
-    onChange={(e) => {
-        setEmail(e.target.value)
-    }}
-    fullWidth
-    />
+      <TextField
+        variant="outlined"
+        type="password"
+        label="Enter password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        fullWidth
+      />
 
-    <TextField 
-    variant='outlined'
-    type='password'
-    label="enter password"
-    value={password}
-    onChange={(e) => {
-        setPassword(e.target.value)
-    }}
-    fullWidth
-    />
+      <TextField
+        variant="outlined"
+        type="Confirm Password"
+        label="Confirm Password"
+        value={confirmPassword}
+        onChange={(e) => {
+          setConfirmPassword(e.target.value);
+        }}
+        fullWidth
+      />
 
-    <TextField 
-    variant='outlined'
-    type='Confirm Password'
-    label="enter Confirm Password"
-    value={confirmPassword}
-    onChange={(e) => {
-        setConfirmPassword(e.target.value)
-    }}
-    fullWidth
-    />
-
-    {/* Signup button */}
-    <Button 
-    variant='contained'
-    size='large'
-    style={{backgroundColor: "#EEBC1D"}}
-    onClick={handleSubmit}
-    >Signup</Button>
+      {/* Signup button */}
+      <Button
+        variant="contained"
+        size="large"
+        style={{ backgroundColor: "#EEBC1D" }}
+        onClick={handleSubmit}
+      >
+        Signup
+      </Button>
     </Box>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
